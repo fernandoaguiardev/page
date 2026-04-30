b3af6180a70649597fc1ddf28b92867e9bddf418
Fernando Aguiar <sigmasolutions.dev@gmail.com>
2026-04-30 20:39:33 -0300

    style:
    -upgrade on header logo

--- js/theme.js
+++
@@ -1,7 +0,0 @@
-/* theme control */
-function toggleTheme() {
-  const html = document.documentElement
-  const isLight = html.getAttribute("data-theme") === "light"
-
-  html.setAttribute("data-theme", isLight ? "dark" : "light")
-}
